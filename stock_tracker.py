26eb0334d89aab5b73ebe130635e28539fc5d56e
Ahmed Uddin <ahmeduddinict@gmail.com>
2020-06-08 15:06:50 -0400

    update features

--- stock_tracker.py
+++ stock_tracker.py
@@ -5,34 +5,41 @@ from urllib.error import HTTPError
 st.sidebar.title('Stock Tracker')
 ticker = st.sidebar.text_input("Enter ticker: ")
 page_title = st.empty()
-ticker = yf.Ticker(ticker)
-
 chart = st.empty()
-time = st.sidebar.selectbox("Time Period", ['5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max'])
+
+time = st.sidebar.selectbox("Time Period", ['YTD', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'MAX'])
 
 
 @st.cache
 # Function used to call Yahoo Finance API for data
 def get_data(search_ticker, period):
-    data = search_ticker.history(period=period)
+    ticker_obj = yf.Ticker(search_ticker)
+    data = ticker_obj.history(period=period)
     return data
 
 
 @st.cache
 # Function to get current stock information
 def get_info(search_ticker=None):
-    data = search_ticker.info
+    ticker_obj = yf.Ticker(search_ticker)
+    data = ticker_obj.info
     return data
 
 
+# Default dictionary keys to retrieve
 keys_to_get = ['open', 'regularMarketDayHigh', 'previousClose', 'trailingPE']
-display_values = ['Open', 'High', 'Close', 'P/E']
-chart.line_chart(get_data(ticker, time).Close)
+display_values = ['Open', 'High', 'Close', 'P/E']  # Formatting key values
 
+# Data requests
 try:
+    chart.line_chart(get_data(ticker, time).Close)
     page_title.title(get_info(ticker)['shortName'])
     for x in range(4):
         info = "".join([display_values[x], ": ", str(get_info(ticker).get(keys_to_get[x]))])
         st.write(info)
 except HTTPError:
+    page_title.title('Please enter a valid ticker name')
+except KeyError:
+    page_title.title('Please enter a valid ticker name')
+except IndexError:
     page_title.title('Please enter a valid ticker name')
